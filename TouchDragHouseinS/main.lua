5bbbd21f7edeefb0dfcea96499f8aef53b261d7a
Housein Shaib <s436317@ocsb.local>
2018-10-03 13:50:58 -0400

    AddedTouchAndDrag

---
+++ TouchDragHouseinS/main.lua
@@ -0,0 +1,12 @@
+-----------------------------------------------------------------------------------------
+-- Title: TouchAndDrag
+-- Name: Housein Shaib
+-- Course: ICS2O
+-- This program displays 2 images that can be clicke and draged
+-----------------------------------------------------------------------------------------
+
+-- Hides the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- Backgrond images with width and height
+local backgroundImage = display.newImageRect("Images/background.png", 2048, 1536)
