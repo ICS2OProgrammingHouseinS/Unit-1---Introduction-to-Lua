f88c00a23df3fc2e1b7e1c65270c083a357b434f
Housein Shaib <s436317@ocsb.local>
2018-10-09 14:30:45 -0400

    1

--- TouchAndReactHouseinS/main.lua
+++ TouchAndReactHouseinS/main.lua
@@ -6,4 +6,54 @@
 -----------------------------------------------------------------------------------------
 
 -- Set background colour
-display.SetDefault("background", 100/255, 255/255, 60/255)
+display.setDefault("background", 255/255, 60/255, 50/255)
+
+-- Hides the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- Creates blue button, sets it's position and, makes it visable.
+local blueButton = display.newImageRect("Images/Fast Button Inactive@2x.png", 198, 96)
+blueButton.x = display.contentWidth/2
+blueButton.y = display.contentHeight/2
+blueButton.isVisible = true
+
+-- Creates red button, set its position and make it invisible
+local redButton = display.newImageRect("Images/Fast Button Active@2x.png", 198, 96)
+redButton.x = display.contentWidth/2
+redButton.y = display.contentHeight/2
+redButton.isVisible = false
+
+-- Creates text object, set it's position and make it invisable
+local textObject = display.newText("PINGU RULES ALL", 0, 0, nil, 67)
+textObject.x = display.contentWidth/2
+textObject.y = display.contentHeight/3
+textObject:setTextColor(0, 1, 1)
+textObject.isVisible = false
+
+-- Function: BlueButtonListener
+-- Input: touch listener
+-- Output: none
+-- Description: When blue button is clicked, it will make the text appear with the red button
+-- and make the blue button disappear
+local function BlueButtonListener(touch)
+	if (touch.phase == "began") then
+		blueButton.isVisible = false
+		redButton.isVisible = true
+		textObject.isVisible = true
+	end
+
+	if (touch.phase == "ended") then
+		blueButton.isVisible = true
+		redButton.isVisible = false
+		textObject.isVisible = false
+	end
+
+end
+
+-- Function: BlueButtonListener
+-- Input: touch listener
+-- Output: none
+-- Description: When
+
+-- Add the respective listeners to each object
+blueButton:addEventListener("touch", BlueButtonListener)
