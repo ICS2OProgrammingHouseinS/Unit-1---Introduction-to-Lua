0648ff4d0b368e0b7d04a638dd3b3a5934d9d796
Housein Shaib <s436317@ocsb.local>
2018-10-09 13:51:44 -0400

    addedTouchAndReact

---
+++ TouchAndReactHouseinS/main.lua
@@ -0,0 +1,9 @@
+-----------------------------------------------------------------------------------------
+-- Title: TouchAndReact
+-- Name: Housein Shaib
+-- Course: ICS2O
+-- This program displays a blue button when clicked displays a red button and an image.
+-----------------------------------------------------------------------------------------
+
+-- Set background colour
+display.SetDefault("background", 100/255, 255/255, 60/255)
