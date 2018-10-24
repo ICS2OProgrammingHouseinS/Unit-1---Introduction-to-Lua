a189593b9cc0e3c2867836d02e46dcc5ee304df6
HouseinShaib <43419051+HouseinShaib@users.noreply.github.com>
2018-10-24 19:59:26 -0400

    1

--- PhysicsAndCollisionsHouseinS/main.lua
+++ PhysicsAndCollisionsHouseinS/main.lua
@@ -34,5 +34,55 @@ local beam = display.newImage("Images/beam_long.png", 0, 0)
 -- Set the x/y value of beam
 beam.x = display.contentCenterX/5
 beam.y = display.contentCenterY*1.65
--- Set width/height
+-- Set the width/height
 beam.width = display.contentWidth/2
+beam.height = display.contentHeight/10
+-- Rotates the beam 45 degrees
+beam:rotate(45)
+-- Sends the beam to back layer
+beam:toBack()
+
+-- Add to physics
+physics.addBody(beam, "static", {friction=0.5, bounce=0.3})
+-----------------------------------------------------------------------------------------
+-- Create bkg
+local bkg = display.newImage("Images/bkg.png", 0, 0)
+
+-- Set the x/y position
+bkg.x = display.contentCenterX
+bkg.y = display.contentCenterY
+-- Set the width/height
+bkg.width = display.contentWidth
+bkg.height = display.contentHeight
+-- Send to back
+bkg:toBack()
+
+-----------------------------------------------------------------------------------------
+-- FUNCTIONS
+-----------------------------------------------------------------------------------------
+
+-- Create the first ball
+local function FirstBall()
+	local ball1 = display.newImage("Images/super_ball.png", 0, 0)
+
+	-- Add to physics
+	physics.addBody(ball1, {density=1.0, friction=0.5, bounce=0.3, radius=25})
+end
+
+-----------------------------------------------------------------------------------------
+
+local function SecondBall()
+	local ball2 = display.newImage("Images/super_ball.png", 0, 0)
+
+	-- Add to physics
+	physics.addBody(ball2, {density=1.0, friction=0.5, bounce=0.3, radius=12.5})
+
+	ball2.xScale = 0.5
+	ball2.yScale = 0.5
+end
+
+-----------------------------------------------------------------------------------------
+-- TIMER DELAYS
+-----------------------------------------------------------------------------------------
+timer.performWithDelay(0, FirstBall)
+timer.performWithDelay(500, SecondBall)
